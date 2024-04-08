5e0253bd5c69323293a24972555c362e95e9a0d1
Matheus Monteiro <math.a.monteiro@gmail.com>
2024-04-08 11:11:37 -0300

    Lab5 - Ping Frontend App

--- generate-keypair/load-keypair.ts
+++ generate-keypair/load-keypair.ts
@@ -4,3 +4,7 @@ import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
 console.log(`The public key is ${keypair.publicKey.toBase58()}`);
+
+
+let secretKeyString = Buffer.from(keypair.secretKey).toString('hex');
+console.log(`The public key is ${secretKeyString}`);
